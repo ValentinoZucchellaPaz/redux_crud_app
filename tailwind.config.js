4fb47fef9ecf85b66f0b5e8470ec28dc7872a62d
Valentino Zucchella Paz <vzucchellapaz@gmail.com>
2025-01-03 16:25:37 -0300

    module problem solved

--- tailwind.config.js
+++ tailwind.config.js
@@ -137,4 +137,6 @@ export const safelist = [
     pattern: /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
   },
 ];
-export const plugins = [require("@headlessui/tailwindcss")];
+
+import headlessui from "@headlessui/tailwindcss"
+export const plugins = [headlessui];
